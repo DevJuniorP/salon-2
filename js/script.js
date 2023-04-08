e3a76a097bd23dfd9dcc86110d8d423ee563e8b2
gilmarperonjunior <103460100+gilmarperonjunior@users.noreply.github.com>
2023-04-08 10:27:35 -0300

    Completo
    08/04/2023

--- js/script.js
+++ js/script.js
@@ -4,3 +4,11 @@ let header = document.querySelector("header");
 menuResponsive.onclick = function () {
     header.classList.toggle('active')
 }
+
+window.onscroll = () => {
+    if(window.scrollY > 60){
+        document.querySelector('#scroll-top').classList.add('active')
+    }else{
+        document.querySelector('#scroll-top').classList.remove('active')
+    }
+}
